98ae875ab8d1b01d46a38324cf110aec6e71fd04
kyh <kyhfan@gmail.com>
2015-12-17 15:06:46 +0900

    111

--- MOBILE/header.php
+++ MOBILE/header.php
@@ -21,17 +21,11 @@
 <title>Babience Awards</title>
 <!--[if lt IE 9]><script src="./js/html5shiv.js"></script><![endif]-->
 <link href="css/style.css" rel="stylesheet" type="text/css">
-    <!-- Generic page styles -->
-    <link rel="stylesheet" href="../lib/jQuery-File-Upload/css/style.css">
-    <!-- CSS to style the file input field as button and adjust the Bootstrap progress bars -->
-    <link rel="stylesheet" href="../lib/jQuery-File-Upload/css/jquery.fileupload.css">
     <link rel="stylesheet" href="../lib/colorbox/colorbox.css">
     <script type="text/javascript" src="../js/jquery-1.11.2.min.js"></script>
     <script type="text/javascript" src="../js/jquery.easing.1.3.js"></script>
     <script type="text/javascript" src="../js/m_main.js"></script>
     <script type="text/javascript" src="../lib/colorbox/jquery.colorbox-min.js"></script>
-    <!-- The jQuery UI widget factory, can be omitted if jQuery UI is already included -->
-    <script src="../lib/jQuery-File-Upload/js/vendor/jquery.ui.widget.js"></script>
     <script src="https://developers.kakao.com/sdk/js/kakao.min.js"></script>
     <script type="text/javascript" src="http://www.youtube.com/player_api"></script>
 
